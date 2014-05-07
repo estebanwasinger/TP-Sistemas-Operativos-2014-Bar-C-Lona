f356429a46bcd2471e18a2f64a3599b3e7950dd9
Juanchi <juandesimoni@gmail.com>
2014-05-07 18:55:30 -0300

    Agragado de hilo para la consola en UMV

--- proceso UMV/src/UMV.c
+++ proceso UMV/src/UMV.c
@@ -1,47 +1,52 @@
+#include"UMV.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void compactar_memoria();
-void switch_function(char* funcion);
+#include <commons/string.h>
+#include <commons/config.h>
+#include <pthread.h>
 
+//Variables Globales de la UMV
+int *memFisica;
+int a = 0;
 
 int main(void) {
+	// Obtenemos la cantidad de memoria y la alocamos
+	int memTotal = ObtenerCantidadMemoriaTotal();
+	if(memTotal <= 0){ printf("Error De Archivo De Config"); return -1;}
+	memFisica = malloc(memTotal);
 
-	char *nombre_funcion = NULL;
-
-	// Aca hacemos el maloc para la string que va a ser el recibidor del comando
-	nombre_funcion = malloc(sizeof(char) * 30);
-
-	if(nombre_funcion == NULL){
-		printf("Fallo al alocar memoria \n");
-	}
-
-	printf("UMV Bar - C - Lona Console. \n\n");
-
-	while(1){
-		scanf("%s",nombre_funcion);
-		if(!strcmp(nombre_funcion, "exit")){
-			printf("\n\n Adios \n");
-			break;
-		}
-		switch_function(nombre_funcion);
-
-	}
-
+	LanzarConsola();
 
 	return EXIT_SUCCESS;
 }
 
-	void switch_function(char *funcion){
-		if(!strcmp(funcion, "compactar")){
-			compactar_memoria();
-			return;
+int ObtenerCantidadMemoriaTotal(){
+
+		// El archivo de configuracion va en la carpeta raiz del proyecto "proceso UMV"
+		t_config * arch_config = config_create("archconfig");
+
+		//Validamos que este la propiedad
+		if (!config_has_property(arch_config, "CANTIDAD_MEM_ALOCAR" ))
+		{
+			puts("Archivo de configuracion incompleto o invalido.\n");
+			return -2;
 		}
 
-		printf("No se encontro el Comando <%s>", funcion);
+		int cantidadMemoria = config_get_int_value(arch_config,"CANTIDAD_MEM_ALOCAR");
 
-	}
+	   return cantidadMemoria;
+}
 
-	void compactar_memoria(){
-		printf("La Compactacion fue exitosa \n");
+void LanzarConsola(){
+    pthread_t hilo_consola;
+    pthread_create(&hilo_consola,NULL, Consola, NULL);
+    pthread_join(&hilo_consola,NULL);
+    printf("%d",a);
+}
+
+void Consola(){
+	while(a<10){
+		a++;
 	}
+ }
